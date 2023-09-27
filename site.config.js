4a5cc1c22af8f2dc0653871edc51ab65f4a7fbcb
starcradle101 <dreamerdev12@gmail.com>
2023-09-27 22:47:46 +0900

    🎨Design: 블로그 아바타 변경 및 프로필 정보 수정

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "starcradle101",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    bio: "Beyond the CODE🧑🏻‍💻",
+    email: "dreamerdev12@gmail.com",
+    linkedin: "https://www.linkedin.com/in/dreamerdev12/",
+    github: "https://github.com/starcradle101",
     instagram: "",
   },
   projects: [
